63c05e56ba8be09672e1b569444978517d6be2f8
Aman Jha <49260887+aman707f@users.noreply.github.com>
2025-12-28 03:00:58 +0530

    Create project_explanation.m

---
+++ adf/sql/data/databricks/databricks/docs/project_explanation.m
@@ -0,0 +1,6 @@
+This project simulates an enterprise data engineering pipeline.
+
+Raw sales data is ingested using Azure Data Factory, transformed using
+Databricks PySpark notebooks, and stored in Delta Lake format for analytics.
+
+The curated data can be consumed by Power BI or Azure Synapse Analytics.
